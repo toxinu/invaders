4da39d4600aa10fc77927b48f9979b2591eafa29
toxinu <toxinu@gmail.com>
2014-07-08 21:14:31 +0200

    Add menu, button. Add quads with images for Entity. And moar

--- classes.lua
+++ classes.lua
@@ -1,5 +1,125 @@
 local class = require 'libs/middleclass'
 
+-- Entity
+local Entity = class('Entity')
+function Entity:initialize(world)
+    self.world = world
+    self.height = 30
+    self.width = 15
+    self.x = 300
+    self.y = 450 - self.height
+    self.speed = 100
+    self.color = {255, 255, 0, 255}
+end
+function Entity:draw()
+    if self.image and self.quad then
+        love.graphics.draw(self.image, self.quad, self.x, self.y)
+    else
+        love.graphics.setColor(self.color)
+        love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+        love.graphics.setColor(255, 255, 255, 255)
+    end
+end
+function Entity:addImage(image_path)
+    self.image = love.graphics.newImage(image_path)
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+    self.quad = love.graphics.newQuad(
+        0, 0,
+        self.image:getWidth(),
+        self.image:getHeight(),
+        self.image:getWidth(),
+        self.image:getHeight())
+end
+function Entity:collide(entity)
+    return  self.x < entity.x + entity.width and
+            entity.x < self.x + self.width and
+            self.y < entity.y + entity.height and
+            entity.y < self.y + self.height
+end
+
+-- Shot
+local Shot = class('Shot', Entity)
+
+-- Player
+local Player = class('Player', Entity)
+function Player:initialize(world)
+    Entity.initialize(self, world)
+    self.x = 200
+    self.y = 520
+    self.speed = 150
+    self.shots = {}
+
+    self:addImage("assets/images/player.png")
+end
+function Player:shot()
+    local shot = Shot:new()
+    shot.color = {255, 255, 255, 255}
+    shot.height = 8
+    shot.width = 2
+    shot.x = self.x + self.width / 2
+    shot.y = self.y
+    table.insert(self.shots, shot)
+end
+function Player:keyreleased(key)
+    if key == " " then
+        self:shot()
+    end
+end
+function Player:update(dt)
+    if love.keyboard.isDown("left") then
+        self.x = self.x - self.speed * dt
+    elseif love.keyboard.isDown("right") then
+        self.x = self.x + self.speed * dt
+    end
+
+    for k, v in pairs(self.shots) do
+        v.y = v.y - v.speed * dt
+        if v.y < 0 then
+            table.remove(self.shots, k)
+        end
+    end
+end
+function Player:draw()
+    Entity.draw(self)
+    for k, v in pairs(self.shots) do
+        v:draw()
+    end
+end
+
+-- Mob
+local Mob = class('Mob', Entity)
+function Mob:initialize(world)
+    Entity.initialize(self, world)
+    self.speed = 40
+
+    self:addImage("assets/images/mob.png")
+end
+function Mob:randomMove(dt)
+    local horizontal_move = math.random(1, 2)
+    if horizontal_move == 1 then
+        self.x = self.x - self.speed * dt
+    elseif horizontal_move == 2 then
+        self.x = self.x + self.speed * dt
+    end
+    local vertical_move = math.random(1, 2)
+    if vertical_move == 1 then
+        self.y = self.y - self.speed * dt
+    elseif vertical_move == 2 then
+        self.y = self.y + self.speed * dt
+    end
+
+    self.y = self.y + self.speed * dt
+end
+function Mob:update(dt)
+    -- self.y = self.y + self.speed * dt
+    self:randomMove(dt)
+
+    if self.y + self.height >= 550 then
+        self.world.loose = true
+    end
+end
+
 -- World
 local World = class('World')
 function World:initialize()
@@ -10,6 +130,29 @@ function World:initialize()
     self.height = 1
     self.stop = false
     self.win = false
+end
+function World:populate()
+    -- Player
+    self:addEntity(Player:new(global.world))
+    -- Mobs
+    local x_offset = 20
+    local y_offset = 50
+    local column = 1
+    for i = 1, 24 do
+        local mob = Mob:new(global.world)
+        mob.width = 40
+        mob.height = 20
+        mob.x = column * (mob.width + x_offset)
+        if mob.x > self.width - mob.width then
+            column = 1
+            y_offset = y_offset + mob.width
+            mob.x = column * (mob.width + x_offset) + x_offset
+        end
+        mob.y = mob.height + y_offset
+        mob.color = {0, 255, 255, 255}
+        self:addEntity(mob)
+        column = column + 1
+    end
 end
 function World:addEntity(entity)
     table.insert(self.entities, entity)
@@ -66,111 +209,6 @@ function World:keyreleased(key)
         if v.keyreleased then
             v:keyreleased(key)
         end
-    end
-end
-
--- Entity
-local Entity = class('Entity')
-function Entity:initialize(world)
-    self.world = world
-    self.height = 30
-    self.width = 15
-    self.x = 300
-    self.y = 450 - self.height
-    self.speed = speed or 100
-    self.color = {255, 255, 0, 255}
-end
-function Entity:draw()
-    if self.image then
-        love.graphics.draw(self.image, self.x, self.y, 0, 1, 1)
-    else
-        love.graphics.setColor(self.color)
-        love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-        love.graphics.setColor(255, 255, 255, 255)
-    end
-end
-function Entity:collide(entity)
-    return  self.x < entity.x + entity.width and
-            entity.x < self.x + self.width and
-            self.y < entity.y + entity.height and
-            entity.y < self.y + self.height
-end
-
--- Shot
-local Shot = class('Shot', Entity)
-
--- Player
-local Player = class('Player', Entity)
-function Player:initialize(world)
-    Entity.initialize(self, world)
-    self.x = 200
-    self.y = 520
-    self.image = love.graphics.newImage("assets/images/player.png")
-    self.color = {0, 255, 255, 255}
-    self.shots = {}
-end
-function Player:shot()
-    local shot = Shot:new()
-    shot.color = {255, 255, 255, 255}
-    shot.height = 8
-    shot.width = 2
-    shot.x = self.x + self.width / 2
-    shot.y = self.y
-    table.insert(self.shots, shot)
-end
-function Player:keyreleased(key)
-    if key == " " then
-        self:shot()
-    end
-end
-function Player:update(dt)
-    if love.keyboard.isDown("left") then
-        self.x = self.x - self.speed * dt
-    elseif love.keyboard.isDown("right") then
-        self.x = self.x + self.speed * dt
-    end
-
-    for k, v in pairs(self.shots) do
-        v.y = v.y - v.speed * dt
-        if v.y < 0 then
-            table.remove(self.shots, k)
-        end
-    end
-end
-function Player:draw()
-    Entity.draw(self)
-    for k, v in pairs(self.shots) do
-        v:draw()
-    end
-end
-
--- Mob
-local Mob = class('Mob', Entity)
-function Mob:initialize(world)
-    Entity.initialize(self, world)
-    self.image = love.graphics.newImage("assets/images/mob.png")
-    self.speed = 30
-end
-function Mob:randomMove(dt)
-    local horizontal_move = math.random(1, 2)
-    if horizontal_move == 1 then
-        self.x = self.x - self.speed * dt
-    elseif horizontal_move == 2 then
-        self.x = self.x + self.speed * dt
-    end
-    local vertical_move = math.random(1, 2)
-    if vertical_move == 1 then
-        self.y = self.y - self.speed * dt
-    elseif vertical_move == 2 then
-        self.y = self.y + self.speed * dt
-    end
-end
-function Mob:update(dt)
-    self.y = self.y + self.speed * dt
-
-    if self.y + self.height >= 550 then
-        self.world.loose = true
-
     end
 end
 
--- main.lua
+++ main.lua
@@ -1,44 +1,71 @@
 local classes = require 'classes'
+local gui = require 'gui'
+
 local Mob = classes.Mob
 local World = classes.World
 local Player = classes.Player
 
+local Menu = gui.Menu
+local Button = gui.Button
+
+
 function love.load()
-    love.window.setTitle('Invaders by socketubs')
-    font = love.graphics.newFont('assets/fonts/superscript.ttf', 50)
-    love.graphics.setFont(font)
-
-    -- World
     global = {}
-    global.world = World:new()
+    global.fonts = {
+        normal = love.graphics.newFont('assets/fonts/superscript.ttf', 50)}
 
-    -- Player
-    global.world:addEntity(Player:new(global.world))
+    love.window.setTitle('Invaders by socketubs')
+    love.graphics.setFont(global.fonts['normal'])
 
-    -- Mobs
-    for i=1,8 do
-        local mob = Mob:new(global.world)
-        mob.width = 40
-        mob.height = 20
-        mob.x = i * (mob.width + 25) + 100
-        mob.y = mob.height + 100
-        mob.color = {0, 255, 255, 255}
-        global.world:addEntity(mob)
+    -- Gamestate
+    -- menu/play
+    global.gamestate = "menu"
+
+    -- Menu
+    global.menu = Menu:new(global)
+    local play_callback = function ()
+        -- World
+        global.world = World:new()
+        global.world:populate()
+        global.gamestate = "play"
     end
+    local exit_callback = function ()
+        love.event.quit()
+    end
+    global.menu:addButton(
+        Button:new("Play", "play", global.fonts['normal'], 100, 400, play_callback))
+    global.menu:addButton(
+        Button:new("Exit", "exit", global.fonts['normal'], 100, 450, exit_callback))
 end
 
 function love.update(dt)
-    global.world:update(dt)
-
     if love.keyboard.isDown("escape") then
-        love.event.quit()
+            love.event.quit()
     end
+    if global.gamestate == "menu" then
+        global.menu:update(dt)
+    else
+        global.world:update(dt)
+    end
+
 end
 
 function love.draw()
-    global.world:draw()
+    if global.gamestate == "menu" then
+        global.menu:draw()
+    else
+        global.world:draw()
+    end
 end
 
 function love.keyreleased(key)
-    global.world:keyreleased(key)
+    if global.gamestate == "play" then
+        global.world:keyreleased(key)
+    end
+end
+
+function love.mousepressed(x, y)
+    if global.gamestate == "menu" then
+        global.menu:mousepressed(x, y)
+    end
 end
